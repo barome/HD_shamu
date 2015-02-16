RazerRom/kernel_lge_hammerhead

cpufreq: add load notify to use with blu_plug
Signed-off-by: engstk <eng.stk@sapo.pt>

drivers/cpufreq/cpufreq_conservative.c
@@ -388,6 +388,8 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 			max_load = load;
 	}
 
+	cpufreq_notify_utilization(policy, max_load);
+	
 	/*
 	 * break out if we 'cannot' reduce the speed as the user might
 	 * want freq_step to be zero

barome/HD_shamu

Conflicts:
	drivers/cpufreq/cpufreq_interactive.c

drivers/cpufreq/cpufreq_conservative.c
@@ -51,6 +51,8 @@ static void cs_check_cpu(int cpu, unsigned int load)
 	struct dbs_data *dbs_data = policy->governor_data;
 	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 
+	cpufreq_notify_utilization(policy, load);
+	
 	/*
 	 * break out if we 'cannot' reduce the speed as the user might
 	 * want freq_step to be zero